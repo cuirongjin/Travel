0897c9ab6c7f4acc7c1dba06867348413f966019
cuirongjin <cuirongjin@126.com>
2019-02-18 16:10:38 +0800

    index-search

---
+++ src/views/city/components/search.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="search">
+     <input type="text" placeholder="训责城市和地区">
+  </div>
+</template>
+<script>
+export default {
+  name:"CitySearch"
+}
+</script>
+<style lang="stylus" scoped>
+.search
+  height .72rem
+  background #00bcd4
+  padding 0 .1rem
+  input 
+    width 100%
+    height .62rem
+    line-height .62rem
+    text-align center
+    padding  0 .1rem 
+    box-sizing border-box
+    border-radius 0.6rem
+    color #666
+</style>
+
+
