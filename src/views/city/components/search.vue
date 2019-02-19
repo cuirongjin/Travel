382e7f6f5290a0a722c1c393fa8f872bcce608a8
cuirongjin <cuirongjin@126.com>
2019-02-19 10:25:05 +0800

    city-search-logic

--- src/views/city/components/search.vue
+++ src/views/city/components/search.vue
@@ -1,11 +1,67 @@
 <template>
-  <div class="search">
-     <input type="text" placeholder="训责城市和地区">
-  </div>
+  <div>
+    <div class="search">
+     <input type="text" placeholder="训责城市和地区" v-model="keyWord">
+    </div>
+    <div
+       class="search-content"
+       ref="search" 
+       v-show="keyWord"
+        >
+      <ul>
+        <li class="search-item border-bottom" v-for="item in list" :key="item.id">{{item.name}}</li>
+        <li  class="search-item border-bottom" v-show="hasList">
+          暂无数据
+        </li>
+      </ul>
+    </div>
+   
+  </div> 
 </template>
 <script>
+import  Bscroll from  'better-scroll'
 export default {
-  name:"CitySearch"
+  name:"CitySearch",
+  props:{
+    cities:Object
+  },
+  data () {
+    return{
+      keyWord:"",
+      list:[],
+      timer:null
+    }
+  },
+  computed:{
+    hasList () {
+      return  !this.list.length
+    }
+  },
+  watch:{
+    keyWord(){
+      if(this.timer){
+        clearTimeout(this.timer)
+      }
+      if(!this.keyWord){
+        this.list=[];
+        return
+      }
+      this.timer=setTimeout(()=>{
+        const  result=[];
+        for (let i in this.cities){
+          this.cities[i].forEach((value)=>{
+            if(value.spell.indexOf(this.keyWord)<-1||value.name.indexOf(this.keyWord)>-1){
+              result.push(value)
+            }
+          })
+        }
+       this.list=result;
+      },100)
+    }
+  },
+  mounted () {
+    this.scroll=new Bscroll(this.$refs.search);
+  }
 }
 </script>
 <style lang="stylus" scoped>
@@ -22,6 +78,20 @@ export default {
     box-sizing border-box
     border-radius 0.1rem
     color #666
+.search-content
+  overflow hidden
+  position absolute
+  top 1.58rem
+  left 0
+  right 0
+  bottom 0
+  background #fff
+  z-index 1
+  .search-item
+    line-height .62rem
+    padding-left .2rem
+    color #666
+    background #fff
 </style>
 
 
