71be724ef9d971eb8a11551b36d6f7079748c21c
cuirongjin <cuirongjin@126.com>
2019-02-22 09:34:44 +0800

    project

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -1,14 +1,46 @@
 <template>
   <div>
     <DetailBanner/>
+    <DetailHeader/>
+    <DetailList :listAll="list"/>
   </div>
 </template>
 <script>
 import  DetailBanner from "./components/Banner"
+import  DetailHeader from "./components/Header"
+import  DetailList from "./components/List"
 export default {
   name:"Detail",
+  data () {
+    return {
+      list:[
+        {
+        title:"成人票",
+        children:[{
+          title:"成人票-海洋馆",
+          children:[{
+            title:"海洋馆-全票"
+          },{
+             title:"海洋馆-半价票"
+          }
+          ]
+        },{
+          title:"成人票-爬虫公馆"
+        }]
+        },
+         {
+        title:"老人票"
+        },
+        {
+        title:"儿童票"
+        },
+      ]
+    }
+  },
   components:{
-    DetailBanner
+    DetailBanner,
+    DetailHeader,
+    DetailList
   }
 }
 </script>
